77c094cd38798dbf6b3cf2016620c26ad10ee7a1
Diogo antonio <diogoantonio47@hotmail.com>
2021-09-15 13:47:10 -0300

    ok

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
 
 function getUserData(){
 
-   
    var usuario = document.getElementById("usuario").value
    var senha = document.getElementById("senha").value
    
-   
-   fetch(`http://localhost/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
+   fetch(`http://afc9-191-55-80-22.ngrok.io/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
    headers: {
      //"Content-Type": "application/json",
-     'Access-Control-Allow-Origin:': '*'
+     "Access-Control-Allow-Origin":"*"
    }})
-   
-
-
-//    let data = {"Username":"diogoantonio","Password":"ogoid369852471","IdSistema":"20"};
-
-//     return fetch("https://publicws.unipam.edu.br/br.edu.unipam.core.unseg.api.Conexao/api/Autenticacao/Acessar", {
-//     method: "POST", 
-//     headers: {
-//         "Content-Type": "application/json",
-//         'Access-Control-Allow-Origin:': '*'
-//       },
-//     body: JSON.stringify(data)
-//     }).then(res => {
-//     console.log(JSON.parse(res))
-//     });
-
-//     req()
 
 }
