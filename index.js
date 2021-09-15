ab1b78df181a518d08ce785a356d301acf5594c4
Diogo antonio <diogoantonio47@hotmail.com>
2021-09-15 16:43:47 -0300

    ok

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getUserData(){
      var usuario = document.getElementById("usuario").value
    var senha = document.getElementById("senha").value
    
-   const teste = () =>  fetch(`https://afc9-191-55-80-22.ngrok.io/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
+   const teste = () =>  fetch(`http://68f8-191-55-80-22.ngrok.io/unimensagem/get.php?usuario=${usuario}&senha=${senha}`, {method: "GET",
    headers: {
      //"Content-Type": "application/json",
      "Access-Control-Allow-Origin":"*",
